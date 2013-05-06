7d00b638171c9571fbe09cf5959a96818c11dfb0
Adam Anderson <beoren@gmail.com>
2013-05-06 15:54:27 -0500

    Updates

--- TplLunchAndLearn.Wpf/MainWindow.xaml.cs
+++ TplLunchAndLearn.Wpf/MainWindow.xaml.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
-using TplLunchAndLearn.Model;
 
 namespace TplLunchAndLearn
 {
@@ -14,6 +15,17 @@ namespace TplLunchAndLearn
         {
             InitializeComponent();
         }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
 
         #region Properties
         private Exception _exception;
@@ -57,18 +69,93 @@ namespace TplLunchAndLearn
         }
         #endregion
 
+        #region Web Helpers
+        public static string ServerPortNumber = "38379";
+
+        public static string BuildUrl(TimeSpan delay, string message)
+        {
+            return string.Format("http://localhost:{0}/home/SlowEcho?delay={1}&input={2}",
+                ServerPortNumber,
+                delay,
+                Uri.EscapeDataString(message));
+        }
+        #endregion
+
+
+
         #region Before TPL
         private void SynchronousBlockCodeMenuItem_Clicked(object sender, RoutedEventArgs e)
         {
-            Thread.Sleep(TimeSpan.FromSeconds(3));
+            var url = BuildUrl(TimeSpan.FromSeconds(5), "Synchronous Test");
+            var result = new WebClient().DownloadString(url);
+            this.Message = result;
         }
 
         private void AsynchronousProgrammingModelMenuItem_Clicked(object sender, RoutedEventArgs e)
         {
-            SlowOperations.AsynchronousProgrammingModel(result =>
+            var url = BuildUrl(TimeSpan.FromSeconds(5), "APM Test");
+            var request = WebRequest.Create(url) as HttpWebRequest;
+            request.BeginGetResponse(new AsyncCallback(AsynchronousProgrammingModelCallback), request);
+        }
+
+        private void AsynchronousProgrammingModelCallback(IAsyncResult asyncResult)
+        {
+            var request = asyncResult.AsyncState as HttpWebRequest;
+            var response = request.EndGetResponse(asyncResult);
+            var responseStream = response.GetResponseStream();
+            var result = new StreamReader(responseStream).ReadToEnd();
+            this.Message = result;
+        }
+
+        private void EventBasedAsynchronousPatternMenuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            var url = BuildUrl(TimeSpan.FromSeconds(5), "EAP Test");
+            var client = new WebClient();
+            client.DownloadStringCompleted += EventBasedAsynchronousPattern_DownloadStringCompleted;
+            client.DownloadStringAsync(new Uri(url));
+        }
+
+        private void EventBasedAsynchronousPattern_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            var result = e.Result;
+            this.Message = result;
+        }
+
+        private void BackgroundWorkerMenuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            var worker = new BackgroundWorker();
+            worker.DoWork += BackgroundWorkerMenuItem_DoWork;
+            worker.RunWorkerCompleted += BackgroundWorkerMenuItem_RunWorkerCompleted;
+            worker.RunWorkerAsync();
+        }
+
+        private void BackgroundWorkerMenuItem_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var url = BuildUrl(TimeSpan.FromSeconds(5), "BackgroundWorker Test");
+            var result = new WebClient().DownloadString(url);
+            e.Result = result;
+        }
+
+        private void BackgroundWorkerMenuItem_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var result = e.Result.ToString();
+            this.Message = result;
+        }
+
+        private void ThreadPoolMenuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            var action = new Action<object>(obj =>
                 {
-                    this.Message = result;
+                    var url = BuildUrl(TimeSpan.FromSeconds(5), "ThreadPool Test");
+                    var result = new WebClient().DownloadString(url);
+
+                    Dispatcher.BeginInvoke(new Action<string>(message =>
+                        {
+                            this.Message = message;
+                        }), result);
                 });
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(action));
         }
         #endregion
 
@@ -76,31 +163,108 @@ namespace TplLunchAndLearn
         private void TaskRunMenuItem_Click(object sender, RoutedEventArgs e)
         {
             var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
-            Task.Run(() =>
-                    {
-                        return SlowOperations.WebClientDownloadString("TaskRun Test");
-                    })
-                .ContinueWith(t =>
-                    {
-                        this.Message = t.Result;
-                    },
-                uiScheduler);
+            var taskAction = new Func<string>(() =>
+                {
+                    var url = BuildUrl(TimeSpan.FromSeconds(5), "TaskRun Test");
+                    var result = new WebClient().DownloadString(url);
+                    return result;
+                });
+            var continuation = new Action<Task<string>>(t =>
+                {
+                    this.Message = t.Result;
+                });
+
+            Task.Run(taskAction)
+                .ContinueWith(continuation, uiScheduler);
         }
 
         private void TaskWaitMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            var task = new Task<string>(() =>
+            var task = Task.Factory.StartNew(() =>
                 {
-                    return SlowOperations.WebClientDownloadString("TaskWait Test");
+                    var url = BuildUrl(TimeSpan.FromSeconds(5), "TaskWait Test");
+                    return new WebClient().DownloadString(url);
                 });
-            task.Start();
-            task.Wait(); // Blocking call
+
+            task.Wait(); // Blocking call!!
+
             var result = task.Result;
             this.Message = result;
+        }
+
+        private void ContinueWhenAnyMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var downloadLetter = new Func<int, string, string>((numberOfSeconds, input) =>
+                {
+                    var url = BuildUrl(TimeSpan.FromSeconds(numberOfSeconds), input);
+                    return new WebClient().DownloadString(url);
+                });
+            var appendLetter = new Func<Task<string>, string>(t =>
+                {
+                    this.Message = string.Format("{0}{1}", this.Message, t.Result);
+                    return t.Result;
+                });
+            var continuation = new Action<Task>(t =>
+                {
+                    this.Message = string.Format("{0}!", this.Message);
+                });
+
+            var tasks = new Task[]
+                {
+                    Task.Factory.StartNew(() => downloadLetter(6, "F" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(4, "O" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(5, "F" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(2, "H" )).ContinueWith(appendLetter)
+                };
+
+            Task.Factory.ContinueWhenAny(tasks, continuation);
+        }
+
+        private void ContinueWhenAllMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var downloadLetter = new Func<int, string, string>((numberOfSeconds, input) =>
+                {
+                    var url = BuildUrl(TimeSpan.FromSeconds(numberOfSeconds), input);
+                    return new WebClient().DownloadString(url);
+                });
+            var appendLetter = new Func<Task<string>, string>(t =>
+                {
+                    this.Message = string.Format("{0}{1}", this.Message, t.Result);
+                    return t.Result;
+                });
+            var continuation = new Func<Task[], string>(t =>
+                {
+                    this.Message = string.Format("{0}!", this.Message);
+                    return this.Message;
+                });
+
+            var tasks = new Task[]
+                {
+                    Task.Factory.StartNew(() => downloadLetter(6, "F" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(4, "O" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(5, "F" )).ContinueWith(appendLetter),
+                    Task.Factory.StartNew(() => downloadLetter(2, "H" )).ContinueWith(appendLetter)
+                };
+
+            Task.Factory.ContinueWhenAll<string>(tasks, continuation);
         }
         #endregion
 
         #region async/await
+        private async void AsyncAwaitExceptionsMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await Task.Factory.StartNew(() =>
+                {
+                    throw new Exception("Hoff-ception");
+                });
+            }
+            catch (Exception ex)
+            {
+                this.Message = ex.Message;
+            }
+        }
         #endregion
 
         #region Exceptions
@@ -117,21 +281,27 @@ namespace TplLunchAndLearn
             var cancellationToken = new CancellationToken();
             var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
 
-            Task.Factory
-                .StartNew(() =>
-                    {
-                        throw new Exception("A sample exception");
-                    })
-                .ContinueWith(t =>
+            var exceptionAction = new Action(() =>
+                {
+                    throw new Exception("A sample exception");
+                });
+            var continuation = new Action<Task>(t =>
                 {
                     if (t.Exception != null)
                     {
                         Dispatcher.BeginInvoke(new Action(() =>
-                        {
-                            throw new Exception("Unobserved exception encountered", t.Exception);
-                        }));
+                            {
+                                throw new Exception("Unobserved exception encountered", t.Exception);
+                            }));
                     }
-                }, cancellationToken, TaskContinuationOptions.OnlyOnFaulted, uiScheduler);
+                });
+
+            Task.Factory
+                .StartNew(exceptionAction)
+                .ContinueWith(continuation,
+                    cancellationToken,
+                    TaskContinuationOptions.OnlyOnFaulted,
+                    uiScheduler);
         }
 
         private void UnobservedTaskExceptionEventMenuItem_Click(object sender, RoutedEventArgs e)
@@ -147,17 +317,6 @@ namespace TplLunchAndLearn
             //        {
             //            TaskScheduler.UnobservedTaskException -= TaskScheduler_UnobservedTaskException;
             //        });
-        }
-        #endregion
-
-        #region INotifyPropertyChanged
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void RaisePropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
         }
         #endregion
     }
